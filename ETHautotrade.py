0c94870413f15aba6d4fb0a24d87e0188f6509fd
GilwonKim <44744753+GilwonKim@users.noreply.github.com>
2021-12-22 22:04:37 +0900

    Update ETHautotrade.py

--- ETHautotrade.py
+++ ETHautotrade.py
@@ -10,29 +10,29 @@ secret = "your secret code"
 myToken = "what is your token"
 
 TargetVolatility = 0.05 #target volatility 5%
-NumOfCoins = 3 #number of coins
+NumOfCoins = 2 #number of coins
 
 def post_message(token, channel, text):
-    """슬랙 메시지 전송"""
+    """send messages to Slack"""
     response = requests.post("https://slack.com/api/chat.postMessage",
         headers={"Authorization": "Bearer "+token},
         data={"channel": channel,"text": text}
     )
 
 def get_target_price(ticker, k):
-    """변동성 돌파 전략으로 매수 목표가 조회"""
+    """breakthorugh price check"""
     df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
     target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
     return target_price
 
 def get_start_time(ticker):
-    """시작 시간 조회"""
+    """get current time"""
     df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
     start_time = df.index[0]
     return start_time
 
 def get_ma5(ticker):
-    """5일 이동 평균선 조회"""
+    """review ma5"""
     df = pyupbit.get_ohlcv(ticker, interval="day", count=5)
     ma5 = df['close'].rolling(5).mean().iloc[-1]
     return ma5
@@ -46,7 +46,7 @@ def get_volatility(ticker):
 
 
 def get_balance(ticker):
-    """잔고 조회"""
+    """volatility of the day"""
     balances = upbit.get_balances()
 
     for b in balances:
@@ -58,12 +58,12 @@ def get_balance(ticker):
     return 0
 
 def get_current_price(ticker):
-    """현재가 조회"""
+    """check balance"""
     return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]
 
 predicted_close_price = 0
 def predict_price(ticker):
-    """Prophet으로 당일 종가 가격 예측"""
+    """forcast the end price with Prophet"""
     global predicted_close_price
     df = pyupbit.get_ohlcv(ticker, interval="minute60")
     df = df.reset_index()
@@ -83,21 +83,20 @@ predict_price("KRW-ETH")
 schedule.every().hour.do(lambda: predict_price("KRW-ETH"))
 
 
-# 로그인
+# log in
 upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
 
-#시작 메세지 슬랙 전송
+# send starting message
 post_message(myToken, "#breakthru", "autotrade start")
 
 
-# 자동매매 시작
+# start trading
 while True:
     try:
         now = datetime.datetime.now()
-        start_time_1 = get_start_time("KRW-ETH")
-        start_time = start_time_1 - datetime.timedelta(hours=8)  #새벽 1시에 매수 시작
-        end_time = start_time_1 + datetime.timedelta(days=1) # 오전 9시에 매도
+        start_time = now.replace(hour=1, minute=0, second=0, microsecond=0)
+        end_time = now.replace(hour=8, minute=30, second=0, microsecond=0)
         schedule.run_pending()
 
         if start_time < now < end_time - datetime.timedelta(seconds=10):
@@ -105,8 +104,6 @@ while True:
             print("Start Time",start_time)
             print("End Time", end_time)
             print("Target Price",target_price)
-            #ma5 = get_ma5("KRW-ETH")
-            #print("ma5",ma5)
             current_price = get_current_price("KRW-ETH")
             print("Current Price",current_price)
             print("Predicted Price",predicted_close_price)
@@ -114,16 +111,22 @@ while True:
             if target_price < current_price and current_price < predicted_close_price:
                 krw = get_balance("KRW")
                 coins = krw / NumOfCoins
-                print("krw")
+                print("KRW", krw)
+                check = get_balance("ETH")
+                print("Num of coins", check)
                 #volatility = get_volatility("KRW-ETH")
                 #print("volatility")
-                if krw > 5000:
-                    buy_result = upbit.buy_market_order("KRW-ETH", coins * 0.9995) #TargetVolatility/volatility/NumOfCoins*krw*0.9995,
+                if check == 0:
+                    buy_result = upbit.buy_market_order("KRW-ETH", coins * 0.9995) #TargetVolatility/volatility/NumOfCoins*krw*0.9995
+                    check = get_balance("ETH")
+                    print("Bought", check)
                     post_message(myToken, "#breakthru", "ETH buy : " +str(buy_result))
         else:
-            eth = get_balance("ETH")
-            if eth > 5000/get_current_price("KRW-ETH"):
-                sell_result = upbit.sell_market_order("KRW-ETH", eth)
+            sell = get_balance("ETH")
+            if sell > 5000/get_current_price("KRW-ETH"):
+                sell_result = upbit.sell_market_order("KRW-ETH", sell)
+                current_price = get_current_price("KRW-ETH")
+                print("OMG sold at",current_price)
                 post_message(myToken, "#breakthru", "ETH sell : " +str(sell_result))
         time.sleep(1)
     except Exception as e:
