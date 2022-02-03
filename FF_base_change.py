b99471b7a64497a96a7c3b9a32424735a5b9107d
GilwonKim <44744753+GilwonKim@users.noreply.github.com>
2022-02-03 18:37:27 +0900

    Update FF_base_change.py

--- FF_base_change.py
+++ FF_base_change.py
@@ -39,49 +39,49 @@ def post_message(token, channel, text):
 
 def get_target_price(ticker, k):
     """breakthorugh price check"""
-    df = get_daily_ohlcv_from_base_new(ticker, base=8) #change your base time to 1:00am
+    df = get_daily_ohlcv_from_base_new(ticker, base=7) #change your base time to 1:00am
     target_price = df.iloc[-2]['close'] + (df.iloc[-2]['high'] - df.iloc[-2]['low']) * k #get value from the second last iloc[-2]
     return target_price
 
 def get_noise_k(ticker):
     """get k value related to noise""" # 20days average noise to get an optimal k value
-    df = get_daily_ohlcv_from_base_new(ticker, base=8) #chage your base time to 1:00am
+    df = get_daily_ohlcv_from_base_new(ticker, base=7) #chage your base time to 1:00am
     noise = (1 - abs((df['open']-df['close'])/(df['high']-df['low']))).rolling(20).mean().iloc[-1] #noise ratio = 1 - (abs(open-close)/(high-low))
     return noise
 
 def get_start_time(ticker):
     """get current time"""
-    df = get_daily_ohlcv_from_base_new(ticker, base=8) #change your base time to 1:00am
+    df = get_daily_ohlcv_from_base_new(ticker, base=7) #change your base time to 1:00am
     start_time = df.index[0]
     return start_time
 
 def get_ma3(ticker):
     """review ma3"""
-    df = get_daily_ohlcv_from_base_new(ticker, base=8) #change your base time to 1:00am
+    df = get_daily_ohlcv_from_base_new(ticker, base=7) #change your base time to 1:00am
     ma3 = df['close'].rolling(3).mean().iloc[-1] #iloc[-1] gets you the very last row value
     return ma3
 
 def get_ma5(ticker):
     """review ma5"""
-    df = get_daily_ohlcv_from_base_new(ticker, base=8) #change your base time to 1:00am
+    df = get_daily_ohlcv_from_base_new(ticker, base=7) #change your base time to 1:00am
     ma5 = df['close'].rolling(5).mean().iloc[-1]
     return ma5
 
 def get_ma10(ticker):
     """review ma10"""
-    df = get_daily_ohlcv_from_base_new(ticker, base=8) #change your base time to 1:00am
+    df = get_daily_ohlcv_from_base_new(ticker, base=7) #change your base time to 1:00am
     ma10 = df['close'].rolling(10).mean().iloc[-1]
     return ma10
 
 def get_ma20(ticker):
     """review ma20"""
-    df = get_daily_ohlcv_from_base_new(ticker, base=8) #change your base time to 1:00am
+    df = get_daily_ohlcv_from_base_new(ticker, base=7) #change your base time to 1:00am
     ma20 = df['close'].rolling(20).mean().iloc[-1]
     return ma20
 
 def get_volatility(ticker):
     """volatility of the day"""
-    df = get_daily_ohlcv_from_base_new(ticker, base=8) #change your base time to 1:00am
+    df = get_daily_ohlcv_from_base_new(ticker, base=7) #change your base time to 1:00am
     volatility = (df.iloc[-1]['high'] - df.iloc[-1]['low']) / df.iloc[-1]['open']
     return volatility
 
@@ -119,7 +119,7 @@ while True:
         now = datetime.datetime.now()
         start_time = now.replace(hour=19, minute=0, second=0, microsecond=0) #start at 7:00pm (2022.01.30)
         start_time_mid = now.replace(hour=0, minute=0, second=0, microsecond=0) #start at 0:0am (2022.01.05)
-        end_time = start_time_mid.replace(hour=8, minute=1, second=0, microsecond=0) #sell everything at 7:01am (2022.01.31)
+        end_time = start_time_mid.replace(hour=7, minute=1, second=0, microsecond=0) #sell everything at 7:01am (2022.01.31)
 
         krw = get_balance("KRW")
         BuyAmount = krw/len(CoinBuyList)
